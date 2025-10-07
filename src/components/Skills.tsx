2b0776f261a6539548171e91ce570837cefd0a2f
B-Sumanth <bsumanth333@gmail.com>
2025-10-07 16:04:48 +0530

    tech1

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills: React.FC = () => {
     programming: ['Java', 'Python', 'C'],
     web: ['HTML', 'CSS'],
     database: ['SQL'],
-    tools: ['Visual Studio Code', 'Git', 'AWS'],
+    tools: ['Visual Studio Code'],
     soft: [
       { name: 'Communication', icon: <MessageCircle className="h-5 w-5" /> },
       { name: 'Teamwork', icon: <Users className="h-5 w-5" /> },
